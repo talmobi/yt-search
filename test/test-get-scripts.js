62c72a82fe4a790f5965779635c73c1b9643baa8
Christian Talmo <talmo.christian@gmail.com>
2020-11-04 17:57:42 +0200

    test: add html script parsing test

---
+++ test/test-get-scripts.js
@@ -0,0 +1,22 @@
+
+const fs = require( 'fs' )
+const path = require( 'path' )
+
+const test = require( 'tape' )
+
+const cheerio = require( 'cheerio' )
+
+const { _getScripts, _findLine, _between } = require( '../src/util.js' )
+
+test( '_getScripts', function ( t ) {
+  const responseText = fs.readFileSync( path.join( __dirname, 'stage', 'jumbled-script-tags.html' ), 'utf8' )
+
+  const scriptsText = _getScripts( responseText )
+
+  const initialData = _between(
+    _findLine( /ytInitialData.*=\s*{/, scriptsText ), '{', '}'
+  )
+
+  t.ok( initialData )
+  t.end()
+} )
