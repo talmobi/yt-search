047beee1e9e121c6a89e1ec5f05f329d58eb2690
Christian Talmo <talmo.christian@gmail.com>
2021-02-08 06:04:46 +0200

    chore: remove old/unusued junk

--- samples/print-playlist-id.js
+++
@@ -1,8 +0,0 @@
-const yts = require( '../src/index.js' )
-
-const opts = { listId: 'PL7k0JFoxwvTbKL8kjGI_CaV31QxCGf1vJ' }
-
-yts( opts, function ( err, playlist ) {
-  console.log( playlist )
-} )
-
